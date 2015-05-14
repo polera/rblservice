b0e353333dcdac09f5cca33dd41b7390dec40044
James Polera <james@uncryptic.com>
2015-05-14 11:29:51 -0400

    Working lookups via goroutines

--- search/search.go
+++ search/search.go
@@ -1,11 +1,17 @@
 package search
 
 import (
-	"fmt"
-	_ "github.com/polera/gorbl"
+	"github.com/polera/gorbl"
 	"log"
 	"sync"
 )
+
+func search(rblList string, targetHost string, results chan<- *gorbl.RBLResults) {
+	res := gorbl.Lookup(rblList, targetHost)
+
+	results <- &res
+
+}
 
 func Run(hostAddress string) {
 
@@ -14,13 +20,39 @@ func Run(hostAddress string) {
 		log.Fatal(err)
 	}
 
-	//results := make(chan *gorbl.RBLResults)
+	results := make(chan *gorbl.RBLResults)
 
 	var waitGroup sync.WaitGroup
 
 	waitGroup.Add(len(lists))
 
 	for _, list := range lists {
-		fmt.Printf("List: %s\n", list.HostName)
+		//	fmt.Printf("List: %s\n", list.HostName)
+		go func(rblList string, targetHost string) {
+			search(rblList, targetHost, results)
+			waitGroup.Done()
+		}(list.HostName, hostAddress)
 	}
+
+	go func() {
+		// Wait for everything to be processed.
+		waitGroup.Wait()
+
+		close(results)
+	}()
+
+	Display(results)
+}
+
+func Display(results chan *gorbl.RBLResults) {
+	// The channel blocks until a result is written to the channel.
+	// Once the channel is closed the for loop terminates.
+	for result := range results {
+		log.Printf("%s\n\n", result.List)
+		for _, listing := range result.Results {
+			log.Printf("%s:\n%v\n%v\n\n", listing.Address,
+				listing.Listed, listing.Text)
+		}
+	}
+	return
 }
