5b3072b8916644f425007fcb295bbbf091b892be
James Polera <james@uncryptic.com>
2015-05-17 00:02:11 -0400

    Added Godeps

--- middleware.go
+++ middleware.go
@@ -4,6 +4,10 @@ import (
 	"net/http"
 )
 
+/*
+JSONResponse sets the appropriate Content-Type header for
+returning JSON data.
+*/
 func JSONResponse(h http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/json")
